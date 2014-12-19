549fddafd7b6634d18ee0e41fbb7e24b1560b7f1
Rafael Coelho <rafaellima@infojr.com.br>
2014-12-19 12:11:53 -0300

    Creates system of battle
    Created: arrays towel1 and towel2, functions dieRoll, totalDamage
    Modified: function battle

--- script.js
+++ script.js
@@ -1,4 +1,32 @@
-var battle = function (strength, armor){
-    'user strict';
-    return strength > armor ? "First" : "Second";
-}
+var dieRoll = function (){
+    'use strict';
+    return Math.ceil(Math.random() * 6);
+};
+
+var towel1 = {
+    strength: 4, 
+    armor: 3, 
+    endurance: 2
+};
+towel1.life = towel1.endurance*5;
+
+var towel2 = {
+    strength: 3, 
+    armor: 1, 
+    endurance: 5
+};
+towel2.life = towel2.endurance*5;
+
+var totalDamage = function (){
+    'use strict';
+    return (towel1.strength * dieRoll()) - (towel2.armor * dieRoll());
+};
+
+var battle = function (){
+    'use strict';
+    while (towel2.life > 0) {
+        towel2.life -= totalDamage();
+        console.log(towel2.life);
+    }
+    return 'MORREU PARCERO';
+};
