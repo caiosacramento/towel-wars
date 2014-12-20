727941c371cf31e83ae406a9077ebcb340680d38
Caio Sacramento <Caiosacramento@outlook.com>
2014-12-20 13:26:58 -0300

    Add modifications of 20/12

---
+++ application.js
@@ -0,0 +1,8 @@
+window.addEventListener('load', function () {
+    'use strict';
+    var dieRoll = function (sides) {
+        return Math.ceil(Math.random() * sides);
+    };
+    
+    
+};
--- script.js
+++ script.js
@@ -1,51 +1,55 @@
-var dieRoll = function () {
+var dieRoll = function (sides) {
     'use strict';
-    return Math.ceil(Math.random() * 6);
+    return Math.ceil(Math.random() * sides);
 };
 
-var character = function (name, pointsLimit) {
+window.addEventListener('load', function () {
     'use strict';
-    do {
-        var towel = {
-            name: name,
-            strength: parseInt(prompt('Força:')),
-            armor: parseInt(prompt('Armadura:')),
-            endurance: parseInt(prompt('Resistência:'))
-        };
-    } while ((towel.armor + towel.endurance + towel.strength) > pointsLimit);
-    return towel;
-};
+    var character = function (pointsLimit) {
+        var name = document.querySelector('#char_name').value,
+            strength = parseInt(document.querySelector('#char_strength').value),
+            armor = parseInt(document.querySelector('#char_armor').value),
+            endurance = parseInt(document.querySelector('#char_endurance').value);
+        if ((strength + armor + endurance) > pointsLimit) {
+            alert(
+                'A soma dos pontos de atributos deve ser igual a ' + pointsLimit + '!'
+            )
+        } else {
+            var towel = {
+                name: document.querySelector('#char_name').value,
+                strength: strength,
+                armor: armor,
+                endurance: endurance
+            };
+        }
+    };
 
-var towel1 = character('Rafael', 20);
-var towel2 = character('Caio', 20);
+    var towel1 = character(20);
+    var towel2 = character(20);
 
-var characters = document.querySelector('.characters');
-node = document.createElement('div');
-character1 = document.createTextNode(towel1.name);
-character2 = document.createTextNode(towel2.name);
-characters.appendChild(node);
-node.appendChild(character1);
-node.appendChild(character2);
+    var characters = document.querySelector('.characters');
+    node = document.createElement('div');
+    character1 = document.createTextNode(towel1.name);
+    character2 = document.createTextNode(towel2.name);
+    characters.appendChild(node);
+    node.appendChild(character1);
+    node.appendChild(character2);
 
-var attack = document.querySelector('button_atk');
-    'use strict'
-button.addEventListener('click', function () {
-    console.log('Character 1 attacks Character 2');
+    var attack = document.querySelector('button_atk');
+    button.addEventListener('click', function () {
+        console.log('Character 1 attacks Character 2');
+    });
+
+    var totalDamage = function (attacker, defender) {
+        return (attacker.strength * dieRoll(6)) - (defender.armor * dieRoll(6));
+    };
+
+    var battle = function (){
+        while (towel1.life > 0 && towel2.life > 0) {
+            towel2.life -= totalDamage(towel1, towel2);
+            towel1.life -= totalDamage(towel2, towel1);
+        }
+        return towel1.life > 0 ? console.log(towel1.name) + ' ganhou' : console.log(towel2.name) + ' ganhou';
+    };
+
 });
-
-/*characters.innerHTML = '<p>' + towel1.name + '</p>';
-characters.innerHTML = '<p>' + towel2.name + '</p>';*/
-
-var totalDamage = function (attacker, defender) {
-    'use strict';
-    return (attacker.strength * dieRoll()) - (defender.armor * dieRoll());
-};
-
-var battle = function (){
-    'use strict';
-    while (towel1.life > 0 && towel2.life > 0) {
-        towel2.life -= totalDamage(towel1, towel2);
-        towel1.life -= totalDamage(towel2, towel1);
-    }
-    return towel1.life > 0 ? console.log(towel1.name) + ' ganhou' : console.log(towel2.name) + ' ganhou';
-};
